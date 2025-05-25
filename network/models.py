ff8d91b37cf80163833df46f8fa6913965016af4
Andrew <andrusha2349@gmail.com>
2025-05-26 01:37:30 +0300

    finally did coursework

--- network/models.py
+++ network/models.py
@@ -62,3 +62,6 @@ class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.post.caption}"
